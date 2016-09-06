c6ec418398686901a215987afec3e4bad96cc7f9
David Asaf <dasaf3@gatech.edu>
2016-09-06 12:47:37 -0400

    Adding IndexRedirect to project.

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, browserHistory, IndexRedirect, DefaultRoute} from 'react-router';
 
 import App from '../../ui/App.jsx';
 import Dashboard from '../../ui/Dashboard/Dashboard.jsx';
@@ -12,6 +12,7 @@ import Widgets from '../../ui/Widgets/Widgets.jsx';
 export const renderRoutes = () => (
 <Router history={browserHistory}>
   <Route path="/" component={App}>
+    <IndexRedirect to="dashboard"/>
     <Route path="dashboard" component={Dashboard}/>
     <Route path="users" component={Users}/>
     <Route path="widgets" component={Widgets}/>
